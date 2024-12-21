b4f459ec30042cd27a5b610dc6084f75ce003ea0
Amadeus Beckmann <amadeus@beckmann.io>
2024-12-21 21:19:47 +0100

    Send events from a background thread

--- dpongpy/remote/centralised/__init__.py
+++ dpongpy/remote/centralised/__init__.py
@@ -7,6 +7,7 @@ from dpongpy.model import Direction, Pong
 from dpongpy.remote.udp import UdpClient, UdpServer, Address
 from dpongpy.remote.presentation import serialize, deserialize
 import threading
+import queue
 
 
 DEFAULT_HOST = "localhost"
@@ -109,6 +110,9 @@ class PongTerminal(PongGame):
         self.client = UdpClient(Address(self.settings.host or DEFAULT_HOST, self.settings.port or DEFAULT_PORT))
         self._thread_receiver = threading.Thread(target=self._handle_ingoing_messages, daemon=True)
         self._thread_receiver.start()
+        self._events_queue = queue.Queue()
+        self._thread_sender = threading.Thread(target=self._process_message_queue, daemon=True)
+        self._thread_sender.start()
 
     def create_controller(terminal, paddle_commands = None):
         from dpongpy.controller.local import PongInputHandler, EventHandler
@@ -120,7 +124,7 @@ class PongTerminal(PongGame):
             def post_event(self, event: Event | ControlEvent, **kwargs):
                 event = super().post_event(event, **kwargs)
                 if not ControlEvent.TIME_ELAPSED.matches(event):
-                    terminal.client.send(serialize(event))
+                    terminal._events_queue.put(event)
                 return event
 
             def handle_inputs(self, dt=None):
@@ -146,6 +150,21 @@ class PongTerminal(PongGame):
             message = deserialize(message)
             assert isinstance(message, pygame.event.Event), f"Expected {pygame.event.Event}, got {type(message)}"
             pygame.event.post(message)
+
+    def _process_message_queue(self):
+        while self.running:
+            try:
+                # Get event from the queue
+                event = self._events_queue.get()
+                if event is None:
+                    break
+
+                # Send the event to the server
+                self.client.send(serialize(event))
+            except Exception as e:
+                print(f"Error sending packet: {e}")
+            finally:
+                self._events_queue.task_done()
 
     def before_run(self):
         super().before_run()
